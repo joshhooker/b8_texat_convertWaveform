73b69f082c179aef6ee3a5465aa5d23081b4a4e6
Josh Hooker <joshualeehooker@gmail.com>
2018-12-19 10:36:12 -0600

    Changed few things to work on QT creator

--- Convert.C
+++ Convert.C
@@ -6,12 +6,15 @@ Double_t Shaper(Double_t *x, Double_t *p) {
     return (x[0] > p[2]) ? p[0] + x[0]*p[5] + semiGaus : p[0] + x[0]*p[5];
 }
 
-TChain* MakeChain();
-
 int main(int argc, char *argv[]) {
 
   // Home
-  TString PathToFiles = "/hd3/research/data/run0817a/rootM2R-WaveformModule/run";
+  // TString PathToFiles = "/hd3/research/data/run0817a/rootM2R-WaveformModule/run";
+  TString PathToFiles = "/hd/research/data/run0817a/rootM2R-WaveformModule/run";
+
+  // Mac Laptop
+  //TString PathToFiles = "/Users/joshhooker/Desktop/run";
+
   PathToFiles += argv[1];
   PathToFiles += ".root";
 
@@ -29,79 +32,6 @@ int main(int argc, char *argv[]) {
   // delete f;
 
   return 0;
-}
-
-TChain* MakeChain() {
-  auto *chain = new TChain("mfmData");
-
-  // Home
-  // TString PathToFiles = "/hd3/research/data/run0817a/rootM2R-WaveformModule/run";
-  TString PathToFiles = "/hd3/research/data/run0817a/rootM2R-WaveformModule/run";
-
-  // Mac Laptop
-  // TString PathToFiles = "/Users/joshhooker/Desktop/data/run0817a/run";
-
-  // Linux Laptop
-  // TString PathToFiles = "/home/joshhooker/Desktop/data/run0817a/waveform/run";
-
-  // Alpha source test in gas
-  // chain->Add(PathToFiles+"004.root");
-
-  // 8B beam with IC
-  // chain->Add(PathToFiles + "158.root");
-
-  // 8B Runs
-  chain->Add(PathToFiles + "175.root");
-  // chain->Add(PathToFiles + "178.root");
-  // chain->Add(PathToFiles + "180.root");
-  // chain->Add(PathToFiles + "181.root");
-  // chain->Add(PathToFiles + "182.root");
-  // chain->Add(PathToFiles + "183.root");
-  // chain->Add(PathToFiles + "184.root");
-  // chain->Add(PathToFiles + "185.root");
-  // chain->Add(PathToFiles + "186.root");
-  // chain->Add(PathToFiles + "187.root");
-  // chain->Add(PathToFiles + "188.root");
-  // chain->Add(PathToFiles + "189.root");
-  // chain->Add(PathToFiles + "190.root");
-  // chain->Add(PathToFiles + "191.root");
-  // chain->Add(PathToFiles + "192.root");
-  // chain->Add(PathToFiles + "193.root");
-  // chain->Add(PathToFiles + "195.root");
-  // chain->Add(PathToFiles + "196.root");
-  // chain->Add(PathToFiles + "197.root");
-  // chain->Add(PathToFiles + "198.root");
-  // chain->Add(PathToFiles + "199.root");
-  // chain->Add(PathToFiles + "200.root");
-  // chain->Add(PathToFiles + "201.root");
-  // chain->Add(PathToFiles + "202.root");
-  // chain->Add(PathToFiles + "203.root");
-  // chain->Add(PathToFiles + "204.root");
-  // chain->Add(PathToFiles + "205.root");
-  // chain->Add(PathToFiles + "206.root");
-  // chain->Add(PathToFiles + "207.root");
-  // chain->Add(PathToFiles + "208.root");
-  // chain->Add(PathToFiles + "210.root");
-  // chain->Add(PathToFiles + "211.root");
-  // chain->Add(PathToFiles + "212.root");
-  // chain->Add(PathToFiles + "213.root");
-  // chain->Add(PathToFiles + "214.root");
-  // chain->Add(PathToFiles + "215.root");
-  // chain->Add(PathToFiles + "216.root");
-  // chain->Add(PathToFiles + "217.root");
-  // chain->Add(PathToFiles + "218.root");
-  // chain->Add(PathToFiles + "219.root");
-  // chain->Add(PathToFiles + "220.root");
-  // chain->Add(PathToFiles + "221.root");
-  // chain->Add(PathToFiles + "223.root");
-  // chain->Add(PathToFiles + "224.root");
-
-  // Alpha source test in vacuum
-
-  // Alpha source test in gas
-  // chain->Add(PathToFiles + "232.root");
-
-  return chain;
 }
 
 void Convert::Loop() {
